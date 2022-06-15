850b1dc7bc2b403dc7289691d2ec24bfa6727aa9
wowanamatsu <antonioflavionte@gmail.com>
2022-06-15 08:08:54 -0300

    v.0.1

--- SpriteAnimation.hpp
+++ SpriteAnimation.hpp
@@ -2,11 +2,58 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 
-class SpriteAnimation {
+class Animate {
 
 private:
+    sf::Sprite sprite;
+    sf::Texture texture;
+    sf::IntRect rectSourceSprite;
+    sf::Clock clock;
 
+    /* 
+        sW              = source width
+        sH              = source height
+        sliceWidth      = width
+        sliceHeight     = altura do quado recortado do sprite
+    */
+    int sW, sH, sliceWidth, sliceHeight;
 
 public:
+
+    Animate( std::string file_name, int _sW=0, int _sH=0, int _sliceWidth=16, int _sliceHeight=16 ) {
+        rectSourceSprite = sf::IntRect( _sW, _sH, _sliceWidth, _sliceHeight );
+        sW = _sW;
+        sH = _sH;
+        sliceWidth = _sliceWidth;
+        sliceHeight = _sliceHeight;
+        texture.loadFromFile( file_name );
+        sprite.setTexture( texture );
+        sprite.setTextureRect( rectSourceSprite );
+    }
+
+    void setScale( float x, float y ) {
+        sprite.setScale( x, y );
+    }
+
+    void render( sf::RenderWindow& window ) {
+        update();
+        window.draw( sprite );
+    }
+
+    void update() {
+        if( clock.getElapsedTime().asSeconds() > .2f) {
+            if( rectSourceSprite.left == width()-sliceWidth ) {
+                rectSourceSprite.left = 0;
+            } else {
+                rectSourceSprite.left += sliceWidth;
+            }
+
+            sprite.setTextureRect( rectSourceSprite );
+            clock.restart();
+        }
+    }
+    
+    int width() { return texture.getSize().x; }
+    int height() { return texture.getSize().y; }
 
 };
--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
-#include <SFML/Graphics.hpp>
+#include "SpriteAnimation.hpp"
 
 
 int main(int argc, char** arvg) {
 
     sf::RenderWindow window( sf::VideoMode(1280, 720), "Sprite Animation");
+    window.setFramerateLimit(60);
     sf::Event e;
+
+    Animate sp("./mage.png", 0, 0, 16, 16);
+    sp.setScale(10, 10);
 
 
     while( window.isOpen() ) {
@@ -16,6 +20,7 @@ int main(int argc, char** arvg) {
     
         window.clear();
     
+        sp.render( window );
     
         window.display();
     }
